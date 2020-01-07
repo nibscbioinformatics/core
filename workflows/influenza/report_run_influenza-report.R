e95a57753d1d9c492689daffd8ee5320a0131904
nibscles <francesco.lescai@nibsc.org>
2020-01-07 18:00:56 +0000

    problems with markdown workdir trying to solve

--- workflows/influenza/report_run_influenza-report.R
+++ workflows/influenza/report_run_influenza-report.R
@@ -4,8 +4,10 @@ args <- commandArgs(TRUE)
 
 library(rmarkdown)
 
+workdir <- getwd()
+
 markdown<-args[1]
 report <-args[2]
 results<-args[-c(1,2)]
 
-render(input = markdown, output_file = report, params = list(data = results))
+render(input = markdown, output_file = report, params = list(data = results), knit_root_dir=workdir, output_dir=workdir)
